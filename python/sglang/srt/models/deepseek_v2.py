5cfb9633259a9d8508f89ecc00e7aed370b9bf6d
RolaoDenthu <xinyis10@illinois.edu>
2026-05-07 22:45:52 +0000

    use_grouped_topk=false for dsv4

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -469,7 +469,9 @@ class DeepseekV2MoE(nn.Module):
             prefix=add_prefix("experts", prefix),
         )
 
-        self.use_grouped_topk = config.n_group > config.topk_group
+        self.use_grouped_topk = (
+            False if is_deepseek_v4 else config.n_group > config.topk_group
+        )
 
         if self.is_hash and not (is_nextn and is_deepseek_v4):
             from sglang.srt.layers.moe.deepseek_v4_topk import HashTopK
